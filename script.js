f6db8775791359352dfecd536a25c3e77c13d045
Samehv9 <sameh.av6@gmail.com>
2023-12-05 01:15:35 +0200

    Portfolio

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 var typed = new Typed(".text", {
-    strings: ["Website Designer." , "Front-End Developer."],
-    typeSpeed:100,
-    backSpeed:100,
-    backDelay:1000,
+    strings: ["Website Designer.", "Front-End Developer."],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
     loop: true
 });
 
